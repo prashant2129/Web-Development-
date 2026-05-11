d877a4f081d9596d48349a3b2b05acec1c375e55
Prashant Rakh <prashantrakh38@gmail.com>
2026-05-11 15:58:03 +0530

    Create  DesignaPHPscripttodisplay"Welcome"messageandcurrentdate&time.php

---
+++ DesignaPHPscripttodisplay"Welcome"messageandcurrentdate&time.php
@@ -0,0 +1,15 @@
+<?php
+
+date_default_timezone_set("Asia/Kolkata");
+
+echo "<h1>Welcome to PHP Programming</h1>";
+
+echo "<h2>Current Date: "
+     . date("d-m-Y") .
+     "</h2>";
+
+echo "<h2>Current Time: "
+     . date("h:i:s A") .
+     "</h2>";
+
+?>
