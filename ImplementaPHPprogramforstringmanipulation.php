936c94f7d9fe931c2107fa6d0126dbfad63dc182
Prashant Rakh <prashantrakh38@gmail.com>
2026-05-11 16:00:18 +0530

    Create ImplementaPHPprogramforstringmanipulation.php

---
+++ ImplementaPHPprogramforstringmanipulation.php
@@ -0,0 +1,17 @@
+<?php
+
+$string = "Hello PHP Programming";
+
+echo "<h2>Original String: " . $string . "</h2>";
+
+echo "Length of String: " . strlen($string);
+
+echo "<br><br>";
+
+echo "Reverse String: " . strrev($string);
+
+echo "<br><br>";
+
+echo "Substring: " . substr($string, 6, 3);
+
+?>
